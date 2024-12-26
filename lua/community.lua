e54e8be8b6050f39a95b7b961d5d8dfd2a443811
oyelowo <oyelowooyedayo@gmail.com>
2024-12-26 18:07:20 +0200

    Disable oil nvim clash with default panes splitting and switching

--- lua/community.lua
+++ lua/community.lua
@@ -134,6 +134,40 @@ return {
 
   { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
+  { "oil.nvim",
+    opts = {
+      -- add any opts here
+      -- provider = "copilot",
+      -- auto_suggestions_provider = "copilot"
+      keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        -- Cmd + h clashes with my default moving to top or bottom pane
+        -- So, switch to Cmd + m. And I already have an horizontal split
+        -- with just `-` but keep Cmd + m for now for redundancy
+        -- ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-m>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        -- Cmd + l clashes with my default moving to left pane
+        -- So, switch to Cmd + r. And I already have moving to 
+        -- left or right panes via just `|` but keep Cmd + r for now
+        -- ["<C-l>"] = "actions.refresh",
+        ["<C-r>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+      },
+      use_default_keymaps = false,
+    }
+  },
 
   { import = "astrocommunity.programming-language-support.csv-vim" },
 
